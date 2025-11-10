46460b08e92d45f68cd2f058d5a0f103d04b7fa2
Bethel Amadi <amadibethel@icloud.com>
2025-11-10 12:11:20 +0200

    Implementing a Custom User Model in Django

---
+++ advanced_features_and_security/LibraryProject/README.md
@@ -0,0 +1,13 @@
+# LibraryProject 📚
+
+## Overview
+LibraryProject is the introductory Django project for the **ALX Django LearnLab**.  
+This task demonstrates the process of setting up a Django environment and running a development server.
+
+---
+
+## Steps to Reproduce
+1. Install Django:
+   ```bash
+   pip install django
+
---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books
+
+urlpatterns = [
+    # ------------------------------
+    # Book and Library Views
+    # ------------------------------
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # ------------------------------
+    # Authentication (Built-in Class-Based Views)
+    # ------------------------------
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register_view, name='register'),
+
+    # ------------------------------
+    # Role-Based Access Views
+    # ------------------------------
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+
+    # ------------------------------
+    # Book CRUD Views (Permission Protected)
+    # ------------------------------
+    path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book_view, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book_view, name='delete_book'),
+]
