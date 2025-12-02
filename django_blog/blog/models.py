283fe346f3f75d41d95e5a6ab889c20649ed3f3b
Bethel Amadi <amadibethel@icloud.com>
2025-12-02 04:26:13 +0200

    Implementing the Blog's User Authentication System

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    published_date = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    class Meta:
+        ordering = ['-published_date']
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug])
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+from .forms import CustomUserCreationForm, UserUpdateForm, ProfileUpdateForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, f"Account created for {user.username}. You can now log in.")
+            return redirect('login')
+        else:
+            # form invalid: fall through to re-render with errors
+            pass
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your profile has been updated.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'blog/profile.html', context)
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
+
