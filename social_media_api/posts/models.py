9db2e54c30ffe1f4b406e36f49f52601391c075c
Bethel Amadi <amadibethel@icloud.com>
2025-12-10 15:47:40 +0200

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,7 +5,10 @@ from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL  # string used by migrations
 
+User = get_user_model()
+
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
     content = models.TextField()
